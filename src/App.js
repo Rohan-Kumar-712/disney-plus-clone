12a1e4a40f88829a87b7d8b61e737147037034a8
Rohan-Kumar-712 <rohankumar7128@gmail.com>
2022-06-02 20:32:52 +0530

    Added the sub-pages

--- src/App.js
+++ src/App.js
@@ -3,19 +3,34 @@ import './App.css';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import Detail from './Components/Detail';
+
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  Routes
 } from "react-router-dom";
 
 function App() {
   return (
 
     <div className="App"> 
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+      <Route path="/">
+          <Home />
+        </Route>
+
+        <Route path="/detail">
+          <Detail />
+        </Route>
+      </Routes>
+      
+    </Router>
+
+    <Home />
      </div>
      
   );
